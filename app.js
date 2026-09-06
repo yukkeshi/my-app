cb8ccba032dd1faca6ec27ac1d6f5b7aab12e99d
yukkeshi <yukkeshi827@gmail.com>
2026-09-06 15:04:23 +0900

    カウントアップボタン搭載

---
+++ app.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const text = document.getElementById("count-text");
+const button = document.getElementById("count-btn");
+
+button.addEventListener("click", function() {
+  count = count + 1;
+  text.textContent = "現在のカウント: " + count;
+});
