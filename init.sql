102fdb11c860cc7307ac9c6435ca1759fd998afe
yukkeshi <yukkeshi827@gmail.com>
2026-09-06 15:38:26 +0900

    SQLiteテーブル設計とgitignoreの設定

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE counts (
+    id INTEGER PRIMARY KEY,
+    count_value INTEGER,
+    created_at TEXT
+);
+
+INSERT INTO counts (id, count_value, created_at) 
+VALUES (1, 2, '2026-09-06');
---
+++ insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO counts (id, count_value, created_at) 
+VALUES (2, 4, '2026-09-06');
