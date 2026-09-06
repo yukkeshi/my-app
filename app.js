2e12ecfdd0a514a368a8c535ee3c3c332abe980a
yukkeshi <yukkeshi827@gmail.com>
2026-09-06 15:04:45 +0900

    加算数を1から2に変更

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const text = document.getElementById("count-text");
 const button = document.getElementById("count-btn");
 
 button.addEventListener("click", function() {
-  count = count + 1;
+  count = count + 2;
   text.textContent = "現在のカウント: " + count;
 });
